2aa8ddc9e55b9442fecde20601c103da270c5c39
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2025-02-09 15:22:55 +0200

    add open by email or username

--- project/settings.py
+++ project/settings.py
@@ -132,3 +132,6 @@ MEDIA_ROOT= BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+AUTHENTICATION_BACKENDS=['accounts.backend.Login_Username_Email']
